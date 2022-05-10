23ee82dd733e49233fafa8ad3fe85e81210d6a10
skdding3 <skdding3@gmail.com>
2022-05-10 13:58:37 +0900

    about detail true

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,20 @@
-export default function About() {
-  return <div>About 입니다.</div>;
+import { useMemo } from "react";
+import { useLocation } from "react-router";
+
+function useQuery() {
+  const { search } = useLocation();
+  return useMemo(() => new URLSearchParams(search), [search]);
 }
+
+const About = () => {
+  let query = useQuery();
+  const showDetail = query.get("detail") === "true";
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>디테일 true 조건 확인</p>
+      {showDetail && <p>detail 값을 true로 설정</p>}
+    </div>
+  );
+};
+export default About;
